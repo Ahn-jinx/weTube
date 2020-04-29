e692efb28bea782f7c64ed1148d06ccf114dad1c
jinx <zibbork@gmail.com>
2020-04-30 00:25:54 +0900

    '2nd'

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+const PORT = 4000;
+
+function hiHome(req, res){
+    res.send('gogogoGo');
+};
+
+function hiProfile(req, res){
+    res.send('This is profile ~')
+}
+
+app.listen(PORT);
+app.get('/',hiHome);
+app.get('/profile',hiProfile)
