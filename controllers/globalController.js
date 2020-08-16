df3e41ddd1cb9586475a672fa53318a80b6c0640
jinx <zibbork@gmail.com>
2020-08-17 01:31:29 +0900

    8/16

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -1,5 +1,8 @@
 export const home = (req,res) => res.render('home.pug', {pageTitle : 'Home'});
-export const search = (req,res) => res.render('search', {pageTitle : 'search'});
+export const search = (req,res) => {
+    const searched = req.query.term
+    res.render('search.pug', {pageTitle : 'search', searched : searched});
+    }
 export const join = (req, res) => res.render('join', {pageTitle : 'join'});
 export const login =(req, res) => res.render('login', {pageTitle : 'login'});
 export const logout =(req, res) => res.render('logout', {pageTitle : 'logout'});
